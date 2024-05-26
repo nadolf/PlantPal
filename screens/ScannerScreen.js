eca99227a5868bba202ec68df9c09e42daf5df74
nadolf <nadolftagoe1@gmail.com>
2024-05-26 16:25:36 -0400

    Camera Permission Alert

--- screens/ScannerScreen.js
+++ screens/ScannerScreen.js
@@ -1,34 +1,39 @@
 import React from "react";
-import { View, TouchableOpacity, Button, Text} from 'react-native';
-import { CameraView,useCameraPermissions } from "expo-camera";
+import { View, TouchableOpacity, Text } from "react-native";
+import { CameraView, useCameraPermissions } from "expo-camera";
 import { styles } from "../styles/ScannerScreenStyles";
 
 export default function Scanner({ navigation }) {
+  const [permission, requestPermission] = useCameraPermissions();
 
-    const [permission, requestPermission] = useCameraPermissions();
-  
-    if (!permission) {
-      return <View />;
-    }
-  
-    if (!permission.granted) {
-      return (
-        <View style={styles.container}>
-          <Text style={{ textAlign: 'center' }}>We need your permission to show the camera</Text>
-          <Button onPress={requestPermission} title="grant permission" />
-        </View>
-      );
-    }
+  if (!permission) {
+    return <View />;
+  }
 
-    return (
-      <View style={styles.container}>
-        <CameraView style={styles.camera}>
-          <View style={styles.buttonContainer}>
-            <TouchableOpacity style={styles.button}>
-              <Text style={styles.text}>Camera</Text>
-            </TouchableOpacity>
-          </View>
-        </CameraView>
-      </View>
+  if (!permission.granted) {
+    Alert.alert(
+      "'PlantPal' Would Like to Access the Camera",
+      "To enable scanner feature",
+      [
+        {
+          text: "Don't Allow",
+          onPress: () => console.log("Cancel Pressed"),
+          style: "cancel",
+        },
+        { text: "Allow", onPress: { requestPermission } },
+      ]
     );
+  }
+
+  return (
+    <View style={styles.container}>
+      <CameraView style={styles.camera}>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.text}>Camera</Text>
+          </TouchableOpacity>
+        </View>
+      </CameraView>
+    </View>
+  );
 }
